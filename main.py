dce1d8c4802100fadcb7adfb11640b5c088c6953
sahnurhusanovic-droid <sahnurhusanovic@gmail.com>
2026-04-20 15:43:16 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,126 +1,43 @@
-import psycopg2
-import csv
-conn = psycopg2.connect(
-    dbname="phonebook",  
-    user="postgres",
-    password="shahnur__08",
-    host="localhost",
-    port="5433"
-)
-cur = conn.cursor()
-cur.execute("""
-CREATE TABLE IF NOT EXISTS phonebook (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(100),
-    phone VARCHAR(20)
-)
-""")
-conn.commit()
+import pygame
 
-#1 Insert fron console
+pygame.init()
 
-def insert_from_console():
-    name = input("Enter name: ")
-    phone = input("Enter phone: ")
+WIDTH, HEIGHT = 600, 400
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Moving Ball")
 
-    cur.execute(
-        "INSERT INTO phonebook (name, phone) VALUES (%s, %s)",
-        (name, phone)
-    )
-    conn.commit()
-    print("Added!")
+# Түстер
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
 
-# 🔹 2. Insert from CSV
-def insert_from_csv():
-    with open("date.csv", "r") as file:
-        reader = csv.reader(file)
-        for row in reader:
-            cur.execute(
-                "INSERT INTO phonebook (name, phone) VALUES (%s, %s)",
-                (row[0], row[1])
-            )
-    conn.commit()
-    print("CSV inserted!")
+# Шар
+x, y = WIDTH // 2, HEIGHT // 2
+RADIUS = 25
+STEP = 20
 
-#3 Update
-def update_contact():
-    name = input("Enter name to update: ")
-    new_phone=input("New phone: ")
-    cur.execute(
-        "UPDATE phonebook SET phone=%s WHERE name=%s",
-        (new_phone,name)
-    )
-    conn.commit()
-    print("Updated!")
-    
-#4 Search name
-def search_by_name():
-    name=input("Search name: ")
-    cur.execute(
-        "SELECT * FROM phonebook WHERE name ILIKE %s",
-        ('%'+name+'%',)
-    )
-    for row in cur.fetchall():
-        print(row)
-        
-#5 Search none prefix
-def search_by_prefix():
-    prefix=input("Phone prefix: ")
-    cur.execute(
-        "SELECT * FROM phonebook WHERE phone LIKE %s",
-        (prefix +'%',)
-    )
-    for row in cur.fetchall():
-        print(row)
-        
-#6 Delete
-def delete_contact():
-    value = input("Enter name or phone: ")
-    cur.execute(
-        "DELETE FROM phonebook WHERE name=%s OR phone=%s",
-        (value,value)
-    )
-    conn.commit()
-    print("Deleted!")
-    
-#7 Show all contacts
-def show_all():
-    cur.execute("SELECT * FROM phonebook")
-    rows = cur.fetchall()
-    if rows:
-        for row in rows:
-            print(row)
-    else:
-        print("No contacts!")
-    
-# MENU
-while True:
-    print("\n1.Add console")
-    print("2.Add CSV")
-    print("3.Update")
-    print("4.Search name")
-    print("5.Search prefix")
-    print("6.Delete")
-    print("7. Show all contacts")
-    print("0.Exit")
-    
-    choice=input("Choose: ")
-    
-    if choice=="1":
-        insert_from_console()
-    elif choice=="2":
-        insert_from_csv()
-    elif choice=="3":
-        update_contact()
-    elif choice=="4":
-        search_by_name()
-    elif choice=="5":
-        search_by_prefix()
-    elif choice=="6":
-        delete_contact()
-    elif choice=="7":
-        show_all()
-    elif choice=="0":
-        break
-cur.close()
-conn.close()
+clock = pygame.time.Clock()
+running = True
+
+while running:
+    screen.fill(WHITE)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT and x - STEP - RADIUS >= 0:
+                x -= STEP
+            if event.key == pygame.K_RIGHT and x + STEP + RADIUS <= WIDTH:
+                x += STEP
+            if event.key == pygame.K_UP and y - STEP - RADIUS >= 0:
+                y -= STEP
+            if event.key == pygame.K_DOWN and y + STEP + RADIUS <= HEIGHT:
+                y += STEP
+
+    pygame.draw.circle(screen, RED, (x, y), RADIUS)
+
+    pygame.display.update()
+    clock.tick(60)
+
+pygame.quit()
